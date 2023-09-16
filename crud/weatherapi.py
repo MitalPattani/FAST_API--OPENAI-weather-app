0d17ef56731d55ef350915024f0f8920d0b91c5d
MitalPattani <mitalpattni@gmail.com>
2023-09-16 18:47:11 +0400

    "langchain update"

--- crud/weatherapi.py
+++ crud/weatherapi.py
@@ -3,7 +3,7 @@ import io
 import os
 import json
 from dotenv import dotenv_values
-
+import pandas as pd
 from os.path import join, dirname
 from dotenv import load_dotenv
 
@@ -14,7 +14,8 @@ Weather_API_key = os.environ.get("Weather_API_key")
 
 ## select Relevant fields for weather APIs
 
-def get_weather_city(location):
+def get_weather_API(location):
+    print(location)
     response = requests.get("http://api.weatherapi.com/v1/current.json?key="+Weather_API_key+"&q="+location)
     if response.status_code == 200:
         data = response.json()
@@ -35,3 +36,40 @@ def get_weather_city(location):
         resp = "No such city!"
 
     return resp
+
+
+def num_compare(x, wr):
+    if x.name=="weather_condition":
+      return x.dropna()[x.dropna().apply(lambda a: wr[x.name].lower() in a)]
+    else:
+      return x.dropna()[pd.eval(str(wr[x.name])+" "+x.dropna().astype(str))]
+
+
+
+## Outfit recommendation logic based on temprature, humidity, weather_condition, wind speed KPH, cloud cover  
+
+def get_outfit_recomendation(weather_response):
+    outfit_logic = pd.read_excel("outfit_logic.xlsx")
+
+    if type(weather_response)== str:
+        weather_response = weather_response.split("=")[-1].strip() 
+        weather_response = json.loads(weather_response)
+
+    
+    outfit_logic = outfit_logic.set_index("outfit")
+    outfit_logic = outfit_logic.apply(lambda x : num_compare(x,weather_response))
+    return outfit_logic.index.tolist()
+
+## activity recommendation logic based on weather conditions
+
+def get_activity_recomendation(weather_response):
+    if type(weather_response)== str:
+        weather_response = weather_response.split("=")[-1].strip() 
+        weather_response = json.loads(weather_response)
+
+    if (weather_response["weather_condition"] in ["cloudy","rain", "thunder"]):
+        return "suggest an indoor activities"
+    
+    else:
+        return "suggest an outdoor activities"
+
