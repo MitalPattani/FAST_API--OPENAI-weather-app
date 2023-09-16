dc03a2e2a05f6358c1e92163036cee5cb2085fc8
MitalPattani <mitalpattni@gmail.com>
2023-09-16 19:00:55 +0400

    langchain implementation

--- main.py
+++ main.py
@@ -1,16 +1,29 @@
 import pandas as pd
 from fastapi import FastAPI, Query
-import crud.openai_suggestions as crud
+# import crud.openai_suggestions as crud_openai
+import crud.langchain_implement as crud_langchain
 import os
 
 app = FastAPI()
 
 
-## Get API to call the LLM Chatbot
+# ## Get API to call the LLM Chatbot
+
+# @app.get("/weather/{Location}")
+# async def get_weather_city(Location: str):
+    
+#     response = await crud_openai.generate_LLM_response(Location)
+#     return response
+
+
+
+
+## Get API to call langchain LLM Chatbot
+
 
 @app.get("/weather/{Location}")
 async def get_weather_city(Location: str):
     
-    response = await crud.generate_LLM_response(Location)
+    response = await crud_langchain.generate_LLM_response(Location)
     return response
 
